f596964211ad7a30d72cbadfcc8059f7fa32e93d
Tyler Fontaine <tyler@tylerfontaine.com>
2015-08-22 15:38:45 -0500

    prov gets app up and running now, though app has errors at runtime to be fixed

--- prov.sh
+++ prov.sh
@@ -7,13 +7,24 @@
 ###Update OS
 sudo yum -y update
 
-##install prereqs
+##install os prereqs
 sudo yum install -y epel-release
 sudo yum groupinstall -y 'development tools'
 sudo yum install -y nodejs curl-devel nano sqlite-devel libyaml-devel nginx git
 
+##Install rvm for ruby, gem versioning
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=rails,unicorn
+source ~/.profile
+
 #setup dirs for rails App
 sudo mkdir -p /opt/app
+sudo chown vagrant:vagrant /opt/app
 
 #clone the git repo to run latest version of App
 git clone https://github.com/railstutorial/sample_app.git /opt/app
+
+#setup rails app
+mv /opt/app/Gemfile.lock /opt/app/Gemfile.lock.old
+cd /opt/app && bundle install
+cd /opt/app && rails s >> /tmp/railsapp.log
